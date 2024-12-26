0f5c03e36a7e1bf549df1b0e3e8c13198b689301
marte <karel-m@seznam.cz>
2024-12-26 17:02:48 +0100

    samohlasky

---
+++ my-very-first-app/src/main.js
@@ -0,0 +1,15 @@
+// samohlásky
+const samohlasky = ["a","e","i","o","u"]
+for (let i = 0; i < samohlasky.length; i++) {
+    console.log(samohlasky[i]);                            //iterujeme přes indexy pole
+}
+
+console.log("-----------------------------------------------------")
+
+const samohlasky1 = ["a", "e", "i", "o", "u"];
+
+for (const samohlaska of samohlasky1) {
+    console.log(samohlaska);
+}
+
+console.log("-----------------------------------------------------")
