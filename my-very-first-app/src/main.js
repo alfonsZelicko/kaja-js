3de8f01c28f85f4b5a34edc8ffdad4b06f487e25
marte <karel-m@seznam.cz>
2024-12-29 21:52:50 +0100

    hledani samohlasek ve vete

--- my-very-first-app/src/main.js
+++ my-very-first-app/src/main.js
@@ -1,18 +1,29 @@
 // samohlásky
-const samohlasky = ["a","e","i","o","u"]
-for (let i = 0; i < samohlasky.length; i++) {
-    console.log(samohlasky[i]);                            //iterujeme přes indexy pole
+function obsahuje_samohlasky(veta) {
+    const samohlasky = "aeiouáéíóúůě"
+    let nalezeneSamohlasky = []
+    for (let znak of veta.toLowerCase()) {
+        if (samohlasky.includes((znak))) {
+            nalezeneSamohlasky.push(znak)
+        }
+    }
+    return nalezeneSamohlasky.length > 0 ? nalezeneSamohlasky : false; // Vrátí pole samohlásek nebo false
 }
+const veta1 = ("Tahle věta obsahuje samohlásky")
+const veta2 = ("nnnnnn rrrrr kkkk")
 
-console.log("-----------------------------------------------------")
+console.log(obsahuje_samohlasky(veta1))
+console.log(obsahuje_samohlasky(veta2))
 
-const samohlasky1 = ["a", "e", "i", "o", "u"];
-
-for (const samohlaska of samohlasky1) {
-    console.log(samohlaska);                                //iterujeme přimo objekty v proměnné
-}
-
-console.log("-----------------------------------------------------")
+// console.log("-----------------------------------------------------")
+//
+// const samohlasky1 = ["a", "e", "i", "o", "u"];
+//
+// for (const samohlaska of samohlasky1) {
+//     console.log(samohlaska);                                //iterujeme přimo objekty v proměnné
+// }
+//
+// console.log("-----------------------------------------------------")
 
 
 
