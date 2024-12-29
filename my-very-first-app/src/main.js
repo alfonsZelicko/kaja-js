41037e370969af22f86e4a0de2a6ee8803fc968a
marte <karel-m@seznam.cz>
2024-12-29 21:31:56 +0100

    rozdělení úkolu do více branches

--- my-very-first-app/src/main.js
+++ my-very-first-app/src/main.js
@@ -1,32 +1,3 @@
-// samohlásky
-const samohlasky = ["a","e","i","o","u"]
-for (let i = 0; i < samohlasky.length; i++) {
-    console.log(samohlasky[i]);                            //iterujeme přes indexy pole
-}
-
-console.log("-----------------------------------------------------")
-
-const samohlasky1 = ["a", "e", "i", "o", "u"];
-
-for (const samohlaska of samohlasky1) {
-    console.log(samohlaska);                                //iterujeme přimo objekty v proměnné
-}
-
-console.log("-----------------------------------------------------")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // sčítání cifer
 const cifernySoucetStr = (cislo) => {
