6a92985f74103bb26136d54217ce1cead01fb58b
marte <karel-m@seznam.cz>
2025-01-04 17:21:55 +0100

    zaciname pocitat od dvou

--- my-very-first-app/src/main.js
+++ my-very-first-app/src/main.js
@@ -2,7 +2,7 @@ let question = prompt("Write a number for factorial count")
 
 function faktorial(number) {
     let zaklad = 1;
-    for (let i = 1; i <= number; i++) {
+    for (let i = 2; i <= number; i++) {
         zaklad = zaklad * i;
     }
     return zaklad
